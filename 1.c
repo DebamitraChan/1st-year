8cc12f6bfeaa59f8fc342fc3f93c44cc33b9e95e
DebamitraChan <168839774+DebamitraChan@users.noreply.github.com>
2024-08-12 08:07:30 +0530

    codes by me

--- 1.c
+++ 1.c
@@ -1,7 +1,5 @@
-# 1st-year
-all the practice questions
 #include<stdio.h>
 int main(){
     printf("Hello World");
-    return 0;
+    re ::turn 0;
 }
---
+++ 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    int a;
+    printf("Enter Number :");
+    scanf("%d",&a);
+
+    int b;
+    printf("Enter Number_2:");
+    scanf("%d",&b);
+     
+
+     int sum =a+b;
+     printf("Enter :%d\n",sum);
+     return 0;
+}
